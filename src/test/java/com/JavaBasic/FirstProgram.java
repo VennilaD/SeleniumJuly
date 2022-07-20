ad74645465f457b2e2bd033b25f66f669ef30dd7
Vennila D <vennila.d.88@gmail.com>
2022-07-20 16:32:03 -0700

    commit on july 2022

---
+++ src/test/java/com/JavaBasic/FirstProgram.java
@@ -0,0 +1,28 @@
+package com.JavaBasic;
+
+public class FirstProgram {
+	
+	int a=5;
+	float x=40.50f;
+	float y=66.55f;
+	double b=55;
+	double c=88;
+	
+	public static int add(int e,int f) {
+		return e+f;
+	}
+		
+		public int sub(int g,int h) {
+			return g-h;
+			
+	}
+	public static void main(String[] args) {
+		System.out.println("This is my first Program");
+		FirstProgram obj=new FirstProgram();
+		System.out.println("value of b is " +obj.b);
+		System.out.println("added value is " +add(5,8));
+		System.out.println("subtracted value is " +obj.sub(8, 2));
+		
+	}
+
+}
